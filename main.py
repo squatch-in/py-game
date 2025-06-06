5585033bfba1564d1ca57af640fdd0dd65e65cfc
jacob <jvwyatt7@gmail.com>
2025-06-05 19:24:48 -0500

    the weeds are getting thick

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ from constants import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    dt = 0
+    
+    print(clock.get_fps())
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,6 +22,7 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
-
+        clock.tick(60)
+        #pygame.font(clock.get_fps())
 if __name__ == "__main__":
     main()
