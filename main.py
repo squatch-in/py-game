c945aa9610ec9531f5a979e9f324fe406ca6e72a
jacob <jvwyatt7@gmail.com>
2025-06-06 23:33:03 -0500

    we got this

--- main.py
+++ main.py
@@ -10,7 +10,11 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     dt = 0 
     player = Player(x=SCREEN_WIDTH / 2, y=SCREEN_HEIGHT / 2)
-    print(clock.get_fps())
+    updatable = pygame.sprite.Group(player)
+    drawable = pygame.sprite.Group(player)
+    Player.containers = (updatable, drawable)
+    print(drawable)
+    
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -21,12 +25,17 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+
+        updatable.update(dt)
+
+        for draw in drawable:
+            draw.draw(screen)
         
-        player.update(dt)
-        player.draw(screen)
+        #player.update(dt)
+        #player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
-        #pygame.font(clock.get_fps())
+        #print(clock.get_fps())
 
 
 if __name__ == "__main__":
