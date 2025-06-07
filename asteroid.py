a2dff04ec357d4aa26db12675b9f9925e76d21e6
jacob <jvwyatt7@gmail.com>
2025-06-07 00:26:23 -0500

    gn

---
+++ asteroid.py
@@ -0,0 +1,9 @@
+from circleshape import *
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        self.x = x
+        self.y = y
+        self.radius = radius
+
+    def draw(self, )
